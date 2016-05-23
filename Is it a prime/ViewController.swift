89ee900829dee5474f8b2b771710082510396fcc
mn <mn@mns-MacBook-Pro.local>
2016-05-23 20:34:14 +0300

    intial commit

--- Is it a prime/ViewController.swift
+++ Is it a prime/ViewController.swift
@@ -9,6 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBAction func isItPrime(sender: AnyObject) {
+        
+        
+        if let number = Int(numberTextField.text!){
+            var isPrime = true
+            
+            if number == 1{
+                isPrime = false
+            }
+            
+            if number != 2 && number != 1{
+                for i in 2..<number{
+                    if number % i == 0{
+                        isPrime = false
+                    }
+                }
+            }
+            //print(isPrime)
+            
+            if isPrime{
+                resultLabel.text = "\(number) is prime!"
+            }else{
+                resultLabel.text = "\(number) is not prime"
+            }
+        }else{
+            resultLabel.text = "Please enter a whole number"
+        }
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
